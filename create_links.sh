1741e37368950067b6467d8e73673b6fad46c786
Jan <j.gabor@me.com>
2018-11-19 19:24:12 +0100

    create_links added

--- create_links.sh
+++ create_links.sh
@@ -1,15 +1,21 @@
-#!/bin/sh
+#!/bin/zsh
+
+# change basename if path differs
 BASE_NAME=~/jg_dotfiles/links
+
 BACKUP_DIR=backup_originals
 
 for file in "$BASE_NAME"/.*
 do
-  if [ -f "$file" ]; then
+  file_name=$(basename $file)
+  
+  # handle files
+  if [ -f "$file" ] || [ -d "$file" ]; then
     file_name=$(basename $file)
     echo $file_name
     
     # If original still exists, backup it..
-    if [ -f ~/"$file_name" ]; then
+    if [ ! -L ~/"$file_name" ]; then
       echo "backing up $file_name.."
       mv ~/"$file_name" $BACKUP_DIR 
     fi
