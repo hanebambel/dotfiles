d2ca334c21546ac12c7089876c922a09135e7f24
Jan <j.gabor@me.com>
2018-11-20 11:47:15 +0100

    bootstrap fixed

--- bootstrap_zsh.sh
+++ bootstrap_zsh.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 BASEDIR=~/jg_dotfiles
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
 if [[ ! -e "$BASEDIR" ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
     mkdir $BASEDIR
+    git clone "https://github.com/hanebambel/dotfiles.git" $BASEDIR
+    zsh -c "$BASEDIR/create_links.sh"
+else
+    echo "Folder $BASEDIR already exists, doing nothing!"
 fi
-git clone "https://github.com/hanebambel/dotfiles.git" $BASEDIR
-zsh -c "$BASEDIR/create_links.sh"
+
