ae58e7d21e2cb39a8d105e1adcde031aaa740138
Jan <j.gabor@me.com>
2018-11-20 12:47:57 +0100

    rollback added

---
+++ rollback.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# change basename if path differs
+BASE_NAME=~/jg_dotfiles/links
+
+BACKUP_DIR=~/jg_dotfiles/backup_originals
+
+for file in "$BASE_NAME"/.*
+do
+    file_name=$(basename $file)
+
+    if [[ -L ~/"$file_name" ]]; then
+        echo "removing link to $file_name"
+        rm ~/"$file_name"
+    fi
+done
+
+cp $BACKUP_DIR/* ~
