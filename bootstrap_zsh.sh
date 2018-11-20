75fa9fbe876a177c6fe00d8201768b601361cd1b
Jan <j.gabor@me.com>
2018-11-20 11:35:01 +0100

    typo

--- bootstrap_zsh.sh
+++ bootstrap_zsh.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-BASEDIR="~/jg_dotfiles"
+BASEDIR=~/jg_dotfiles
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
