ee31173c4eede6610f63b976b149a40492a5c062
Jan <j.gabor@me.com>
2018-12-11 07:04:18 +0100

    better ignores

--- bootstrap_zsh.sh
+++ bootstrap_zsh.sh
@@ -5,6 +5,7 @@ if [[ ! -e "$BASEDIR" ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
     mkdir $BASEDIR
     git clone "https://github.com/hanebambel/dotfiles.git" $BASEDIR
     zsh -c "$BASEDIR/create_links.sh"
